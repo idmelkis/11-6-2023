83c1511ec13a536d186b8e101aee854f636009a1
Ingmārs Meļķis <122742982+idmelkis@users.noreply.github.com>
2023-09-07 09:19:53 +0300

    1. Nodarbība

---
+++ 1_nodarbiba.py
@@ -0,0 +1,25 @@
+#print("11-6")
+x = 5
+y = "ASd"
+z = True
+
+# Izvada tekstu
+"""
+print("y = " + y + ", " + "z = " + str(z))
+print(f"y = {y}, z = {str(z)}")
+"""
+print("y = " + y + ", " + "z = " + str(z))
+print(f"y = {y}, z = {str(z)}")
+
+print("""Rinda
+   Rinda2""")
+print("Rinda3\nRinda4\n4234")
+
+x = "Ievade: "
+y = input(x)
+print(y)
+
+# Ievade: <lietotāja ievade>
+# Izvade: "Vārds: <lietotāja ievade>"
+print("Vārds: " + y)
+print(f"Vārds: {y}")
