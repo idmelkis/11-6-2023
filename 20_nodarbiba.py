87193ce91c76608d7c67daca8afcae89bfb96e78
Ingmars Melkis <idmelkis@edu.riga.lv>
2024-03-28 08:49:22 +0200

    ini faila uzdevums

---
+++ 20_nodarbiba.py
@@ -0,0 +1,10 @@
+# 1. Uzdevums
+# Nolasiet datus no dotā faila 
+# Formāts
+# {Date: {year: 2024, date: 25, month: 03}, ...}
+# Un saglabājiet tos vārdnīcā.
+# Izvadat šo vārdnīcu
+# Piezīmes: 
+# 1. Izmantojat .split() funckiju
+# 2. Lai atrastu noteiktu simbola pozīciju tekstā var izmantot
+# .index('<simbols>') funkciju, kas atgriež simbola pozīciju
